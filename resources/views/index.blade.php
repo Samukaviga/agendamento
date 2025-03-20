b4a4cad120ce63cc2df43c483dcc751f60e40a92
samuel <samuelgomes101112@gmail.com>
2025-03-20 17:27:04 -0300

    adicionando

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Agendamento</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body class="flex justify-center items-center h-screen bg-gray-100">
+
+    <div class="flex flex-col justify-center items-center rounded-lg p-6 bg-white shadow-md">
+        <div class="p-4">01</div>
+        <div class=" p-4">02</div>
+        <div class=" p-4">03</div>
+
+        <button class="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800 mt-4">
+            Submit
+        </button>
+    </div>
+
+</body>
+
+</html>
